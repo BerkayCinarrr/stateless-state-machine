f64fe0ef2c55a5a495dd067bcede2e3c76505e30
bcinar <berkay@kloia.com>
2022-02-12 14:24:38 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stateless_state_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
